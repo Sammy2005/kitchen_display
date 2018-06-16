5b2fe80912144091b4f51e07440d38272165ffd3
Sammy2005 <mbuguanjane2005@gmail.com>
2018-06-16 14:46:50 +0300

    Implementing a template engine

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
 	res.render('index');
