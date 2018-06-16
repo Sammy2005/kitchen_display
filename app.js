53c795c055c79ca22cbdc5dfc6991cfc918274e9
Sammy2005 <mbuguanjane2005@gmail.com>
2018-06-16 09:51:31 +0300

    Server is setup and complete

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.listen(3000, function(){
+	console.log("Server is running on port 3000");
+})
